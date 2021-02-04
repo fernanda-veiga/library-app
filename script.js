86630d3f2de6710f188e09be9acfbea78e77577c
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-02-04 14:08:33 -0300

    Added a object constructor called Book that stores the book's title, author, number of pages and if it was read and displays info about the book. Added a addBookToLibrary function that adds the created book into an array called myLibrary.

---
+++ script.js
@@ -0,0 +1,25 @@
+let myLibrary = [];
+
+//Book object constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        if (this.read == "yes") {
+            return `${this.title} by ${this.author}, ${this.pages} pages, completed`;
+        }
+        else {
+            return `${this.title} by ${this.author}, ${this.pages} pages, not completed`;
+        } 
+    }
+}
+
+function addBookToLibrary() {
+    title = prompt("What is the book title?");
+    author = prompt("Who is the book author?");
+    pages = prompt("How many pages are there in the book?");
+    read = prompt("Have you read the book?");
+    myLibrary.push(new Book(title, author, pages, read));
+}
